70fee74cca59a354cabc13cbe077c913db094355
Mateus Furquim <furquim@devgrid.co.uk>
2022-10-29 17:43:00 -0300

    Implemented test_row_count and test_price_range

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def go(config: DictConfig):
                 parameters={
                     "input_artifact": config["etl"]["input_artifact"],
                     "output_artifact": config["etl"]["output_artifact"],
-                    "output_artifact": config["etl"]["output_artifact"],
                     "output_type": config["etl"]["output_type"],
                     "output_description": config["etl"]["output_description"],
                     "min_price": config["etl"]["min_price"],
@@ -64,10 +63,17 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                entry_point="main",
+                parameters={
+                    "csv": config["data_check"]["csv"],
+                    "ref": config["data_check"]["ref"],
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import scipy.stats
 
 
@@ -31,10 +31,13 @@ def test_column_names(data):
 
 
 def test_neighborhood_names(data):
+    """
+    Test if there are all known neighborhood names in the dataset
+    """
 
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
 
-    neigh = set(data['neighbourhood_group'].unique())
+    neigh = set(data["neighbourhood_group"].unique())
 
     # Unordered check
     assert set(known_names) == set(neigh)
@@ -44,7 +47,7 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = data["longitude"].between(-74.25, -73.50) & data["latitude"].between(40.5, 41.2)
 
     assert np.sum(~idx) == 0
 
@@ -54,12 +57,21 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     Apply a threshold on the KL divergence to detect if the distribution of the new data is
     significantly different than that of the reference dataset
     """
-    dist1 = data['neighbourhood_group'].value_counts().sort_index()
-    dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
+    dist1 = data["neighbourhood_group"].value_counts().sort_index()
+    dist2 = ref_data["neighbourhood_group"].value_counts().sort_index()
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Test if the dataset size is neither too small nor too large
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if the feature price is within the interval [min_price,max_price]
+    """
+    assert data["price"].between(min_price, max_price).all()
